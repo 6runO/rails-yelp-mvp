99177bb75d0fae3b403c184252f2e7869bfc581a
brunogonzaga <babsgonzaga@gmail.com>
2021-10-21 19:07:17 -0300

    Restaurant app done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'cleaning up database...'
+Restaurant.destroy_all
+puts 'database is clean!'
+
+puts "Creating Restaurants..."
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w(chinese italian japanese french belgian).sample
+  )
+  puts "Restaurant #{restaurant.id} is created."
+end
+puts "Restaurants created!"
